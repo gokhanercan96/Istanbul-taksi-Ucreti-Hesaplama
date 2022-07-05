fc4236edcaa9c906da7f187c45c89f3c5c413cf1
gokhanercan96 <107113396+gokhanercan96@users.noreply.github.com>
2022-07-05 23:14:08 +0300

    Add files via upload

---
+++ Istanbul_Taxi_Price_PerKm/src/TaksiPriceCalculator.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class TaksiPriceCalculator {
+    public static void main(String[] args) {
+        double perKm = 6.30d, start = 9.80d, total;
+        int km;
+
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("Taksi Ücretini Öğrenmek İstediğiniz Mesafe (km): ");
+        km = inp.nextInt();
+
+        total = (km * perKm)+ start;
+        total = (total<28)? 28 : total;
+        System.out.print("Ödemeniz Gereken Ücret (Tl):" + total);
+    }
+}
